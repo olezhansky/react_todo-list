71bf6db384653009b2e845367105705cbdf04958
Oleg Ivanov <u014@Altera2.local>
2021-06-14 16:54:49 +0300

    modified

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,4 +1,4 @@
-import {Switch, Route } from 'react-router-dom'
+import {Switch, Route, Redirect } from 'react-router-dom'
 import AddTodo from '../AddTodo/AddTodo'
 import TodoList from '../TodoList/TodoList'
 import NoTodos from '../../assets/NoTodos/NoTodos'
@@ -20,6 +20,7 @@ const Main = () => {
                 <Route path="/basket">
                     <Basket />
                 </Route>
+                <Redirect to="/todolist" />
             </Switch>
         </>
     )
